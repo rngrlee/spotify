87fbb6fdeab7e32020e4322d218bc5c6cd8e69aa
Ranger Lee <mrrangerlee@gmail.com>
2024-03-01 00:19:54 -0800

    added search bar

--- server/server.js
+++ server/server.js
@@ -7,6 +7,29 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    console.log('hi')
+    const spotifyApi = new spotifyWebApi({
+        redirectUri: 'http://localhost:5173',
+        clientId: '6d9ae14686314ea58e6633bc1a060d3f',
+        clientSecret: '2026add7537e4922bc5f44ee995d4bae',
+        refreshToken,
+    })
+
+    spotifyApi
+        .refreshAccessToken()
+        .then(data => {
+            res.json({
+                accessToken: data.body.accessToken,
+                expiresIn: data.body.expiresIn
+            })
+        }).catch((err) => {
+            console.log(err)
+            res.sendStatus(400)
+        })
+})
+
 app.post('/login', (req, res) => {
     const code = req.body.code
     const spotifyApi = new spotifyWebApi({
